fcfd5dfbc92d05e72a28a49fba31c3e3138b3654
jzProg <john_z94@hotmail.com>
2020-11-29 21:53:14 +0200

    refactor: add user indicator on leaderboard page

--- src/components/home/Leaderboard.js
+++ src/components/home/Leaderboard.js
@@ -6,13 +6,16 @@ import User from './User';
 class Leaderboard extends Component {
 
   render() {
+    const { users, username } = this.props;
+
     return (
       <div>
         <ul>
-          { Object.values(this.props.users).sort((user1, user2) => Object.keys(user1.answers).length + user1.questions.length < Object.keys(user2.answers).length + user2.questions.length)
+          { Object.values(users).sort((user1, user2) => Object.keys(user1.answers).length + user1.questions.length < Object.keys(user2.answers).length + user2.questions.length)
                  .map(user => <User username={user.id}
                                     key={user.id}
                                     image={user.avatarURL}
+                                    isYou={username === user.id}
                                     questionsAsked={user.questions.length}
                                     questionsAnswered={Object.keys(user.answers).length}/>)
           }
@@ -22,9 +25,10 @@ class Leaderboard extends Component {
   }
 }
 
-function mapStateToProps({ users }) {
+function mapStateToProps({ users, authed }) {
   return {
-    users
+    users,
+    username: authed.username
   }
 }
 
--- src/components/home/User.js
+++ src/components/home/User.js
@@ -2,13 +2,14 @@ import { faQuestion, faReply } from '@fortawesome/free-solid-svg-icons';
 import Score from './Score';
 
 function User(props) {
-   const { username, image, questionsAsked, questionsAnswered } = props;
+   const { username, image, questionsAsked, questionsAnswered, isYou } = props;
 
    return (
      <div className="user">
        <div className="userDiv">
          <img src={image} alt="Avatar" className="avatar"/>
          <label className="username">{username}</label>
+         { isYou  && (<span className="userIndicator">You</span>) }
        </div>
        <div className="questionDiv">
         <Score faIcon={faQuestion} color={'orange'} scoreValue={questionsAsked}/>
